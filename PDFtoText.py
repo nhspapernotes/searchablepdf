0ac34f45759d3fedb9e29e85f8b7ac7387d270c3
JJmistry <jayminm22@gmail.com>
2018-01-27 17:00:23 +0000

    converts pdfs to raw text

---
+++ PDFtoText.py
@@ -0,0 +1,30 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+def detect_document(path):
+    """Detects document features in an image."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+    return document.text
+
+files = ['Example.png', 'Example2.png', 'Example3.jpg', 'Example4.jpg']
+
+for file in files:
+    a = os.path.join(os.path.dirname(__file__), file)
+    text = detect_document(a)
+    fh = open(file+"output.txt", "w")
+    fh.write(text)
+    fh.close()
+
+
