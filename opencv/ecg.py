df6bb5234894b5500c052c3a82a38bcbc5ed2c4a
unknown <kieran.tilley@icloud.com>
2018-01-28 11:31:33 +0000

    Renamed

--- opencv/ecg.py
+++ opencv/ecg.py
@@ -5,16 +5,14 @@ Returns False if image represents anything else
 
 import cv2
 
-TEMPLATE = cv2.imread('./opencv/ecg_tmplt2.png', 0)
-METHODS = ['cv2.TM_CCOEFF_NORMED', 'cv2.TM_SQDIFF_NORMED']
+def is_ecg(image_path):
 
-for i in range(4):
+    TEMPLATE = cv2.imread('./opencv/ecg_tmplt2.png', 0)
 
-    img = cv2.imread(f'./opencv/not_ecgs/{i+1}.jpg', 0)
-    #img = cv2.imread(f'./opencv/ecgs/ECG summary-0{i + 1}.jpg',0)
+    img = cv2.imread(image_path, 0)
     img2 = img.copy()
     img = img2.copy()
-    
+
     ccoeff_res = cv2.matchTemplate(img, TEMPLATE, eval('cv2.TM_CCOEFF_NORMED'))
     cc_min_val, cc_max_val, cc_min_loc, cc_max_loc = cv2.minMaxLoc(ccoeff_res)
 
@@ -22,8 +20,6 @@ for i in range(4):
     sq_min_val, sq_max_val, sq_min_loc, sq_max_loc = cv2.minMaxLoc(sqdiff_res)
 
     if cc_max_val > 0.35 and sq_max_val < 1.0:
-        print('Yes this is an ECG')
+        return True
     else:
-        print('Not an ECG')
-
-    print('--------------')
+        return False
